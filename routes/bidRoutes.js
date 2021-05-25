c8f1eedc61ea7a597e310128f0b8e54d2767a150
Mia Cabrera <antemia.kavillari@gmail.com>
2021-05-25 08:16:49 -0500

    removed proxy

--- routes/bidRoutes.js
+++ routes/bidRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Bid = require('../models/bidModel')
+const Bid = require('../models/bidModel');
 
-
-
+router.route("/bid").post((req, rest) => {
+    const bid = req.body.bid;
+    const newBid = new Bid({
+        bid
+    })
+    newBid.save(); 
+})
 
 module.export = router;
--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'; 
 import {Modal, Button} from 'react-bootstrap';
+import axios from 'axios'
 
 
 function BidModal() {
@@ -7,6 +8,31 @@ function BidModal() {
   
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [input, setInput] = useState({
+        bid: ''
+    });
+
+    function handleChange(event) {
+        const {name,value} =event.target; 
+
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                [name]: value 
+            }
+
+        }
+    )}
+
+    function handleClick(event) {
+        event.preventDefault();
+        const newBid = {
+            bid: input.bid 
+        }
+        
+        axios.post('http://localhost:4000/bid', newBid)
+    }
   
     return (
       <>
@@ -24,14 +50,13 @@ function BidModal() {
             <Modal.Title>Create Bid</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            I will not close if you click outside me. Don't even try to press
-            escape key.
+            <input onChange={handleChange} value={input.bid} name="bid" autoComplete="off" className="form-control" placeholder="$" />
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close Bid
             </Button>
-            <Button variant="primary">Confirm Bid</Button>
+            <Button variant="primary" onClick={handleClick}>Confirm Bid</Button>
           </Modal.Footer>
         </Modal>
       </>
