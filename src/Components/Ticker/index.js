57e97395a7651a0338e43242a6d1c073969fe416
Mia Cabrera <antemia.kavillari@gmail.com>
2021-06-27 10:46:20 -0500

    @apicase service global state

--- src/Components/Ticker/index.js
+++ src/Components/Ticker/index.js
@@ -1,66 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Row, Container, Table} from 'react-bootstrap'; 
-// import { TickerModel } from '../utils/tickerModel'; 
+import Intraday from '../../API/alphaAPI';
+import AlphaAPI from '../../API/alphaAPI';
 import './style.css';
 
 
-function Ticker() {
+const Ticker = () => {
+    useEffect(() => {
+        const fetch = async () => {
+        const { success, result } = await AlphaAPI.getAll("IBM")
+        console.log(`success: ${success}`);
+        console.log(result);
+        console.log(Intraday)
+        };
+        fetch();
+    }, []);
 
-const [data, setData] = useState([]);
-const [stocks, setStocks] = useState({
-    metaData: {
-        symbol: "",
-        high: "",
-        low: "",
-        open: "",
-        close: "",
-        adjClose: ""
-    }
-});
+// const [data, setData] = useState([]);
+// const [stocks, setStocks] = useState([]);
 
-const api = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo';
+// const api = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo';
 
-const getData = async() => {
-    const stockData = await fetch(`${api}`)
-        .then(res => res.json())
-        setStocks(stockData)
-        console.log(stockData)
+// async function getData() {
+//     const stockData = await fetch(`${api}`)
+//         .then(res => res.json())
+//         setStocks(stockData)
+//         console.log(stockData)  
+//     };
 
-    let adj = data.push(stockData);
-    console.log(adj)
+// useEffect(() => {
+//     getData(); 
+// }); 
 
-    for (let daily of Object.keys(adj)) {
-        setData(daily)
-        console.log(daily) 
-
-        for (let stock of Object.keys(daily)) {
-            setData(stock)
-            console.log(stock)
-        }
-    }
-    return getData; 
-};
-
-useEffect(() => {
-    getData();
-}); 
-
+// let dailyAdj = JSON.stringify(stocks); 
+ 
 
 return(
         <div className="ticker">
         <Container fluid>
             <Row>
                 <h1 className="company-name">
-                   {JSON.stringify(stocks)}
+                   {/* {dailyAdj} */}
                 </h1>
                 <div className="stock-data">
-                    {/* {stocks.map((stock) => {
-                        return(
-                            <div>
-                                <li>{stock.data.metaData.open}</li>
-                            </div>
-                        )
-                    })} */}
                 </div>
                 <Table striped bordered hover size="sm">
                 <thead>
@@ -70,19 +52,14 @@ return(
                         <th>Close</th>
                         <th>High</th>
                         <th>Low</th>
-                    <th>Volume</th>
-                    <th>Adjusted Close</th>
-                    <th>Dividend Amount</th>
+                        <th>Volume</th>
+                        <th>Adjusted Close</th>
+                        <th>Dividend Amount</th>
                     </tr>
                 </thead> 
-                <tbody>
-                    <tr>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                    </tr>
-                </tbody>
+                    <tbody>
+                    <td></td>
+                    </tbody>
                 </Table>
             </Row>
         </Container>
